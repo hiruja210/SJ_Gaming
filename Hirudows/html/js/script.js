1d89948713c1db150154f8f0c4707e910c6ecbed
hiruja210 <103011661+hiruja210@users.noreply.github.com>
2022-04-21 21:48:42 +0530

    Add files via upload

---
+++ Hirudows/html/js/script.js
@@ -0,0 +1,7 @@
+function assign(){
+    let hiruja = document.querySelector('#inp').value
+    // window.open('stp-two.html')
+    // self.open('hiruja.html')
+}
+
+console.log('hi')
---
+++ Hirudows/js/desktop.js
@@ -0,0 +1,3 @@
+function image() {
+    document.getElementsByClassName('win-lgo').src = "../img/Folders-OS-Windows-8-Metro-icon"
+}
